29eadec7c407de932e8871c9ae85b298abe0aeb8
Lightency <100479259+Lightency@users.noreply.github.com>
2022-03-07 14:46:20 +0100

    Create script-edge.sh

---
+++ Ubuntu/script-edge.sh
@@ -0,0 +1,34 @@
+sudo -s #the command need root privileges to work
+
+#checking for updates
+apt-get update
+# install java
+apt install default-jre
+
+#download .jar file from gihtub
+
+
+# creating directory for edge
+mkdir /usr/lib/openems/edge
+mv -t openems-edge.jar /usr/lib/openems/edge
+# creating systemd service
+mkdir /etc/openems/edge.d
+nano /etc/systemd/system/openems/edge.service
+echo "
+[Unit]
+Description=OpenEMS Edge 
+After=network.target 
+[Service]
+User=root 
+Group=root
+Type=notify 
+WorkingDirectory=/usr/lib/openems/edge
+ExecStart=/usr/bin/java -Dfelix.cm.dir=/etc/openems/edge.d/ -jar /usr/lib/openems/edge/openems-edge.jar 
+SuccessExitStatus=143 
+Restart=always 
+RestartSec=10 
+WatchdogSec=60 
+[Install]
+WantedBy=multi-user.target" >> edge.service
+systemctl daemon-reload
+systemctl restart edge --no-block ; journalctl -lfu edge
